52b13265cfa122797df189d503a4adc5e08a118c
tarun_upadhyay <2020kucp1019@iiitkota.ac.in>
2021-07-17 22:32:14 +0530

    Javascript initialised

---
+++ script.js
@@ -0,0 +1,44 @@
+const mealsEl = document.getElementById("meals");
+const favoriteContainer = document.getElementById("fav-meals");
+const mealPopup = document.getElementById("meal-popup");
+const mealInfoEl = document.getElementById("meal-info");
+const popupCloseBtn = document.getElementById("close-popup");
+
+const searchTerm = document.getElementById("search-term");
+const searchBtn = document.getElementById("search");
+
+getRandomMeal();
+fetchFavMeals();
+
+async function getRandomMeal() {
+  const resp = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/random.php"
+  );
+  const respData = await resp.json();
+  const randomMeal = respData.meals[0];
+
+  addMeal(randomMeal, true);
+}
+
+async function getMealById(id) {
+  const resp = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id
+  );
+
+  const respData = await resp.json();
+  const meal = respData.meals[0];
+
+  return meal;
+}
+
+async function getMealsBySearch(term) {
+  const resp = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/search.php?s=" + term
+  );
+
+  const respData = await resp.json();
+  const meals = respData.meals;
+
+  return meals;
+}
+
